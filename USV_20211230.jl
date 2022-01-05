cd2da5a387aeeaaf321bcfb8867f54220102615c
jaehongpark-neuro <jaehongpark.neuro@gmail.com>
2022-01-05 09:57:51 -0500

    add USV_20211230

--- USV_20211230.jl
+++ USV_20211230.jl
@@ -1,8 +1,10 @@
 using GLMakie
 
+
 include("usv_temp.jl")
 
-filename = "F:\\USV\\20211230\\PAG-RAm-ChR2(R)-headfix-sqeuak-ketamine-after_2021-12-30_0002.h5"
+
+filename = "F:\\USV\\20211221\\NTS-RAm\\NTS-RAm-ChR2(L)-headfix-cont-2s_2021-12-21_0001.h5"
 
 
 sp,laser,air_down,time_down = loading(filename);
@@ -12,4 +14,5 @@ sp,laser,air_down,time_down = loading(filename);
 # plot_usv(sp,laser,air_down,time_down)
 
 # within a function
-f = plot_usv(sp,laser,air_down,time_down,3)
+f = plot_usv(sp,laser,air_down,time_down,10)
+save("F:\\labmeeting\\2022\\2022-1st\\20211227\\NTS-RAm-2s-cont.png",f)
